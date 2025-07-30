628716a8bec63bf271cfaee8bd6b279a6c11084e
Aashis <12200199@students.koi.edu.au>
2025-07-30 11:58:11 +1000

    fixed the bg pic

--- src/Conponents/Proflie.jsx
+++ src/Conponents/Proflie.jsx
@@ -1,12 +1,14 @@
 import HomePage from "../pages/HomePage";
+import HeroSection from "../assets/HeroSection.png"
 
 1
 const Profile = () =>{ 
     return (
         <section className='flex-1 bg-[#222]'>
             <div className="background">
-                <img src="https://ryancv.bslthemes.com/developer/wp-content/uploads/sites/3/2022/07/dev-bg-4-1200x744.jpg" className = 'w-full max-h-[500px] rounded-lg'/>
+                <img src={HeroSection} className = 'w-full max-h-[500px] rounded-lg'/>
             </div>
+             
             <HomePage />
         </section>
     )
